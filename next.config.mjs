ed135d3f0208a5e4fb66a8ec8911f0ddb9bf481b
ricardoggz <ricardo_ggzicm@outlook.com>
2024-06-08 18:34:40 -0600

    se cambia la configuracion en archivo de configuracion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:'export',
+    // Configura la carpeta donde se encuentran las páginas
+    /*pagesDir: './src/pages',*/
+    generateStaticParams(){
+        return {
+          '/': { page : '/'},  
+        }
+      },
+};
 
 export default nextConfig;
