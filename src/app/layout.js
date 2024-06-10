a0a1a609018910d5629acb6f340dea6b1238b6bf
ricardoggz <ricardo_ggzicm@outlook.com>
2024-06-09 22:57:20 -0600

    se cambia la fuente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import './globals.css'
-import { Inter } from "next/font/google"
 import { Navigation } from "@/components"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Nivel Producción Audiovisual",
@@ -12,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navigation/>
         {children}
       </body>
