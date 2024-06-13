19a3440ac6046bd011e34b7421505ee8f95b7294
ricardoggz <ricardo_ggzicm@outlook.com>
2024-06-12 22:41:52 -0600

    se agregan enlaces en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.css'
+import { IoMdMail } from "react-icons/io"
+import { FaFacebook, FaWhatsapp, FaYoutube  } from "react-icons/fa"
 
 export const Footer = ()=>{
     return (
@@ -6,19 +8,42 @@ export const Footer = ()=>{
             <div className={`paddingSection maskContainer ${styles.footerMask}`}>
                 <div className={styles.footerContent}>
                     <ul className={styles.footerList}>
-                        <li className={styles.footerTitle}>Contacto</li>
-                        <li>Facebbok</li>
-                        <li>WhatsApp</li>
+                        <li className={styles.footerTitle}>Social</li>
+                        <li>
+                            <a
+                                href='https://www.facebook.com/profile.php?id=100029087456642'
+                                target='_blank'
+                            >
+                                <FaFacebook />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='https://www.youtube.com/@nivelproduccionaudiovisual9266'
+                                target='_blank'
+                            >
+                                <FaYoutube />
+                            </a>
+                        </li>
                     </ul>
                     <ul className={styles.footerList}>
-                        <li className={styles.footerTitle}>Contacto</li>
-                        <li>Facebbok</li>
-                        <li>WhatsApp</li>
-                    </ul>
-                    <ul className={styles.footerList}>
-                        <li className={styles.footerTitle}>Contacto</li>
-                        <li>Facebbok</li>
-                        <li>WhatsApp</li>
+                        <li className={styles.footerTitle}>Contacto directo</li>
+                        <li>
+                            <a
+                                href='https://wa.me/525584405634'
+                                target='_blank'
+                            >
+                                <FaWhatsapp />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='mailto:produccionesnivel@hotmail.com'
+                                target='_blank'
+                            >
+                                <IoMdMail />
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div className={styles.footerCopyRight}>
