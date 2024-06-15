07264e31f4ce84f0bdfdc8a942810bcc4cf73102
ricardoggz <ricardo_ggzicm@outlook.com>
2024-06-15 09:51:07 -0600

    se agregan los enlaces en la informacion de contacto

--- src/app/contacto/components/Contact/Contact.jsx
+++ src/app/contacto/components/Contact/Contact.jsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.css'
+import { FaWhatsapp } from "react-icons/fa6"
+import { CiFacebook, CiMail, CiLocationOn, CiPhone } from "react-icons/ci"
 
 export const Contact = ()=>{
     return (
@@ -11,8 +13,48 @@ export const Contact = ()=>{
                 </figure>
                 <div>
                     <center>
-                        <span>Datos de contacto</span>
+                        <span className={styles.contactTitle}>Datos de contacto</span>
                     </center>
+                    <ul className={styles.contactList}>
+                        <li>
+                            <a
+                                href='https://wa.me/525584405634'
+                                target='_blank'
+                                className={styles.contactLink}
+                            >
+                                <FaWhatsapp /> <span>55 8440 5634</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='https://www.facebook.com/profile.php?id=100029087456642'
+                                target='_blank'
+                                className={styles.contactLink}
+                            >
+                                <CiFacebook /> <span>Nivel Producción Audiovisual</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href='mailto:produccionesnivel@hotmail.com'
+                                target='_blank'
+                                className={styles.contactLink}
+                            >
+                                <CiMail /> <span>produccionesnivel@hotmail.com</span>
+                            </a>
+                        </li>
+                        <li className={styles.contactLink}>
+                            <CiLocationOn /> <span>Venustiano Carranza, CDMX</span>  
+                        </li>
+                        <li>
+                            <a
+                                href='callto:5584405634'
+                                className={styles.contactLink}
+                            >
+                                <CiPhone /> <span>55 8440 5634</span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
